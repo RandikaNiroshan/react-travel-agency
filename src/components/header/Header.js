e2490bd5c5bade13a7dd06368d5e0458584422e6
Randika <randikaniroshan@gmail.com>
2022-07-08 09:53:38 +0530

    Hero section

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import './Header.css';
-
+import React from "react";
+import "./Header.css";
+import { Logo } from "../../assets";
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="header">
+      <div className="header__logo">
+        <img src={Logo} alt="logo" />
+      </div>
+      <div className="header__right">
+        <ul>
+          <li>Services</li>
+          <li>Destinations</li>
+          <li>Book</li>
+          <li>Testimonials</li>
+          <li>Login</li>
+          <li className="header__right-btn">Sign up</li>
+          <select className="header_right-select">
+            <option value="en">EN</option>
+            <option value="lk">LK</option>
+          </select>
+        </ul>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- src/containers/hero/Hero.js
+++ src/containers/hero/Hero.js
@@ -1,13 +1,57 @@
-import React from 'react';
-import { Header } from '../../components';
-import './Hero.css';
+import React from "react";
+import { Header } from "../../components";
+import "./Hero.css";
+import { BiPlay } from "react-icons/bi";
+import {
+  DecoBackground,
+  ImgTraveler,
+  IconPlane,
+  DecoRedSpray,
+} from "../../assets";
 
 const Hero = () => {
   return (
-    <div>
+    <div className="hero">
+      <div className="hero__background">
+        <img src={DecoBackground} alt="bg" />
+      </div>
       <Header />
+      <div className="hero__main">
+        <div className="hero__left">
+          <img src={DecoRedSpray} alt="Highlight" />
+          <p>Best Destinations around the world</p>
+          <h1>
+            Travel, enjoy
+            <br />
+            and live a new
+            <br />
+            and full life
+          </h1>
+          <p>
+            Built Wicket longer admire do barton vanity itself do in it.
+            <br />
+            Preferred to sportsmen it engrossed listening. Park gate
+            <br />
+            sell they west hard for the.
+          </p>
+          <div className="hero__left-cta">
+            <div className="hero__left-cta-btn">Find out more</div>
+            <div className="hero__left-cta-play">
+              <div>
+                <BiPlay />
+              </div>
+              <p>Play Demo</p>
+            </div>
+          </div>
+        </div>
+        <div className="hero__right">
+          <img src={ImgTraveler} alt="Traveler" />
+          <img src={IconPlane} alt="Plane" />
+          <img src={IconPlane} alt="Plane" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
