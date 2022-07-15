9801caab28a89bc630ebc88587021efd8316c20c
Randika <randikaniroshan@gmail.com>
2022-07-15 15:10:27 +0530

    Hero section responsive

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -9,15 +9,15 @@ const Header = () => {
       </div>
       <div className="header__right">
         <ul>
-          <li>Services</li>
-          <li>Destinations</li>
-          <li>Book</li>
-          <li>Testimonials</li>
-          <li>Login</li>
+          <li className="nav-menu">Services</li>
+          <li className="nav-menu">Destinations</li>
+          <li className="nav-menu">Book</li>
+          <li className="nav-menu">Testimonials</li>
+          <li className="nav-menu">Login</li>
           <li className="header__right-btn">Sign up</li>
           <select className="header_right-select">
             <option value="en">EN</option>
-            <option value="lk">LK</option>
+            <option value="si">SI</option>
           </select>
         </ul>
       </div>
